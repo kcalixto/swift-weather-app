e8d678ed596b60b35729cfbdbd14cdcc32c313b7
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-08-19 16:06:20 -0300

    initial commit

---
+++ swift-weather-app/swift_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_weather_appApp.swift
+//  swift-weather-app
+//
+//  Created by Kaua Calixto on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
