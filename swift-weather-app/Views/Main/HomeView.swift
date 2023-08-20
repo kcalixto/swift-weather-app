679a3f1e1174975eabb3153734b450d580fc4592
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-08-19 21:32:04 -0300

    done first 8 chapters

--- swift-weather-app/Views/Main/HomeView.swift
+++ swift-weather-app/Views/Main/HomeView.swift
@@ -5,4 +5,118 @@
 //  Created by Kaua Calixto on 19/08/23.
 //
 
-import Foundation
+import SwiftUI
+import BottomSheet
+
+enum BottomSheetPosition: CGFloat, CaseIterable {
+    case top = 0.83 // 702/844
+    case middle = 0.385 // 325/844
+}
+
+struct HomeView: View {
+    @State var bottomSheetPosition: BottomSheetPosition = .middle
+    @State var bottomSheetTranslation: CGFloat = BottomSheetPosition.middle.rawValue
+    var bottomSheetTranslationProrated: CGFloat {
+        (bottomSheetTranslation - BottomSheetPosition.middle.rawValue) / (BottomSheetPosition.top.rawValue - BottomSheetPosition.middle.rawValue)
+    }
+    @State var hasDragged: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            GeometryReader { geometry in
+                let screenHeight = geometry.size.height + geometry.safeAreaInsets.top + geometry.safeAreaInsets.bottom
+                let imageOffset = screenHeight + 36
+                
+                ZStack {
+                    // MARK: Background Color
+                    Color.background
+                        .ignoresSafeArea()
+
+                    // MARK: Background Image
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+        //            MARK: House Image
+                    Image("House")
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .padding(.top, 257)
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    VStack(spacing: -10 * (1-bottomSheetTranslationProrated)){
+                        Text("Diadema")
+                            .font(.largeTitle)
+                        
+                        VStack{
+                            Text(attributedString)
+                            
+                            Text("H:24º     L18º")
+                                .font(.title3.weight(.semibold))
+                                .opacity(1 - bottomSheetTranslationProrated)
+                            
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 51)
+                    .offset(y: -bottomSheetTranslationProrated * 46)
+                    
+                    //            MARK: Bottom Sheet
+                    BottomSheetView(position: $bottomSheetPosition){
+//                        debug log
+//                        Text(bottomSheetTranslationProrated.formatted())
+                    } content: {
+                            ForecastView(bottomSheetTranslationProrated: bottomSheetTranslationProrated)
+                    }
+                    .onBottomSheetDrag { translation in
+                        bottomSheetTranslation = translation / screenHeight
+                        
+                        withAnimation(.easeInOut){
+                            if bottomSheetPosition == BottomSheetPosition.top {
+                                hasDragged = true
+                            } else {
+                                hasDragged = false
+                            }
+                        }
+                    }
+        
+                    //            MARK: Tab Bar
+                    TabBar(action: {
+                        bottomSheetPosition = .top
+                    })
+                    .offset(y: bottomSheetTranslationProrated * 115)
+                }
+            }
+            .navigationBarHidden(true)
+        }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19º" + (hasDragged ? " | " : "\n ") + "Mostly Clear")
+            
+        if let temp = string.range(of: "19º") {
+            string[temp].font = .system(size: (96 - (bottomSheetTranslationProrated * (96 - 20))), weight: hasDragged ? .semibold : .thin)
+            string[temp].foregroundColor = hasDragged ? .secondary : .primary
+        }
+
+        if let pipe = string.range(of: " | "){
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary.opacity(bottomSheetTranslationProrated)
+        }
+        
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        
+        return string
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .preferredColorScheme(.dark)
+    }
+}
