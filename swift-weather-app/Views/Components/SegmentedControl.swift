307a4c407a7b4b1cbe5cb817154dcefa8262a176
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-08-19 22:01:37 -0300

    added divider

---
+++ swift-weather-app/Views/Components/SegmentedControl.swift
@@ -0,0 +1,20 @@
+//
+//  SegmentedControl.swift
+//  swift-weather-app
+//
+//  Created by Kaua Calixto on 19/08/23.
+//
+
+import SwiftUI
+
+struct SegmentedControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SegmentedControl_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentedControl()
+    }
+}
